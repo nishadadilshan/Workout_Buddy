ebaee4230fbffd1eff89b83d33b26aab3f5699dd
SYNAPSYS\dilshanw <dilshan.weerathunga@sysnapsys.lk>
2024-02-20 23:25:24 +0530

    added dotenv to hire port number

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,15 @@
+require("dotenv").config();
+
 const express = require("express");
 //express app
 const app = express();
 
+//routes
+app.get("/", (req, res) => {
+  res.json({ message: "welcome to app" });
+});
+
 //listen for requests
-app.listen(4000, () => {
-  console.log("listening on port 4000");
+app.listen(process.env.PORT, () => {
+  console.log("listening on port ", process.env.PORT);
 });
