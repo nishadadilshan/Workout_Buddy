7838df64d74441118bcab28410cdb9f2f4b734b4
SYNAPSYS\dilshanw <dilshan.weerathunga@sysnapsys.lk>
2024-03-17 19:30:59 +0530

    Created workout module and upated post request for add the workouts

--- backend/routes/workout.js
+++ backend/routes/workout.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const Workout = require("../module/workoutModule");
 
 //GET all workouts
 router.get("/", (req, res) => {
@@ -12,8 +13,16 @@ router.get("/:id", (req, res) => {
 });
 
 //POST a new workout
-router.post("/", (req, res) => {
-  res.json({ message: "Post a new Workouts" });
+router.post("/", async (req, res) => {
+  const { title, reps, load } = req.body;
+
+  try {
+    const workout = await Workout.create({ title, reps, load });
+    res.status(200).json(workout);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+  // res.json({ message: "Post a new Workouts" });
 });
 
 //DELETE a workout
