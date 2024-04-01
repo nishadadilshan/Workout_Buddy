ef5e0a0e5d9e5faf0b45996660e70cc51537369c
SYNAPSYS\dilshanw <dilshan.weerathunga@sysnapsys.lk>
2024-04-02 00:32:01 +0530

    changed css

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -2,27 +2,17 @@ import React from "react";
 
 export default function WorkoutDetails({ workout }) {
   return (
-    <div
-      className="workout-details"
-      style={{
-        borderRadius: "8px",
-        borderBottom: "1px solid",
-        borderBottomColor: "blue",
-      }}
-    >
-      <h3 style={{ color: "black" }}>{workout.title}</h3>
-      <p style={{ color: "gray" }}>
-        <strong>Load (Kg): </strong>
+    <div className="workout-details">
+      <h4>{workout.title}</h4>
+      <p>
+        <strong>Load (Kg):</strong>
         {workout.load}
       </p>
-      <p style={{ color: "gray" }}>
-        <strong>Reps: </strong>
+      <p>
+        <strong>Reps:</strong>
         {workout.reps}
       </p>
-      <p style={{ color: "gray" }}>
-        <strong>Created Date: </strong>
-        {workout.createdAt}
-      </p>
+      <p>{workout.createdAt}</p>
     </div>
   );
 }
